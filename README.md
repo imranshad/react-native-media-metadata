82afa274484307a63c1118a09dcc991aa8dde953
imran shad <imran@goally.co>
2025-05-07 14:29:47 +0500

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# react-native-media-metadata
+
+This is a powerful React Native module for extracting metadata and thumbnails from media files. It supports audio and video files, allowing you to retrieve essential metadata such as album name, artist, genre, and creation date, as well as video frame and artwork thumbnails
+
+## Installation
+
+```sh
+npm install react-native-media-metadata
+```
+
+## Usage
+
+
+```js
+import { multiply } from 'react-native-media-metadata';
+
+// ...
+
+const result = multiply(3, 7);
+```
+
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ android/src/main/java/com/mediametadata/MediaMetadataPackage.kt
@@ -0,0 +1,33 @@
+package com.mediametadata
+
+import com.facebook.react.BaseReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
+import java.util.HashMap
+
+class MediaMetadataPackage : BaseReactPackage() {
+  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+    return if (name == MediaMetadataModule.NAME) {
+      MediaMetadataModule(reactContext)
+    } else {
+      null
+    }
+  }
+
+  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+    return ReactModuleInfoProvider {
+      val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
+      moduleInfos[MediaMetadataModule.NAME] = ReactModuleInfo(
+        MediaMetadataModule.NAME,
+        MediaMetadataModule.NAME,
+        false,  // canOverrideExistingModule
+        false,  // needsEagerInit
+        false,  // isCxxModule
+        true // isTurboModule
+      )
+      moduleInfos
+    }
+  }
+}
